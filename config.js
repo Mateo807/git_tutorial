436f515c2c417a1b87c19351808eb34194c56682
Matthew Banda <mateyobanda2002@gmail.com>
2025-06-05 22:44:49 +0200

    version 1 restored

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 console.log("we are in config.js");
-console.log("version3");
+console.log("version1");
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 console.log("we are in index.js")
-console.log("version3");
+console.log("version1");
